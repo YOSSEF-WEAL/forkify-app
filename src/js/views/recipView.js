dc3e8a172082c9bc1a1b19c921e9986eb304914a
YOSSEF-WEAL <yweal423@gmail.com>
2025-02-19 15:07:39 +0200

    Update after npm i svg 5

--- src/js/views/recipView.js
+++ src/js/views/recipView.js
@@ -1,6 +1,8 @@
 import View from './view.js';
-// import icons from 'url:../../img/icons.svg';
-import icons from new URL('../../img/icons.svg', import.meta.url);
+import icons from 'url:../../img/icons.svg';
+
+
+
 // import { Fraction } from 'fractional';
 import Fraction from 'fraction.js';
 
@@ -126,7 +128,7 @@ class RecipeView extends View
     return `  
         <li class="recipe__ingredient">
               <svg class="recipe__icon">
-                <use href="${icons}#icon-check"></use>
+                <use href="/img/icons.svg#icon-clock"></use>
               </svg>
               <div class="recipe__quantity">${ing.quantity ? new Fraction(ing.quantity).toString() : ''}</div >
     <div class="recipe__description">
@@ -138,4 +140,6 @@ class RecipeView extends View
   }
 
 }
+// <use href="${icons}#icon-check"></use>
+
 export default new RecipeView();
